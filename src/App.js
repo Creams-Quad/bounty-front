3e1ab154406ab4d2534ee7c210e241be2766d42d
Sunny Lee <sunnylee3712@gmail.com>
2021-08-16 20:46:21 -0700

    added Auth0 to the landing page on login button onclick

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import { withAuth0 } from "@auth0/auth0-react";
+import { withAuth0, useAuth0 } from "@auth0/auth0-react";
 import Landingpage from './components/Landingpage/Landingpage';
-
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Bounties  from './components/bounties/bounties';
 
 function App() {
+const {user, isAuthenticated} = useAuth0(); 
 
   return (
-    
     <div className="App">
-      <Navbar ></Navbar>
+      <Navbar></Navbar>
       <Landingpage/>
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isAuthenticated ? <Bounties/> : null}
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-  <Auth0Provider
-    domain="dev-b086be-1.us.auth0.com"
-    clientId="xb6hJdhwZDkXMBaN4PgnWCLwvsn1Mrmu"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
+  <BrowserRouter>
+    <Auth0Provider
+      domain={process.env.REACT_APP_DOMAIN}
+      clientId={process.env.REACT_APP_CLIENT_ID}
+      redirectUri={window.location.origin}
+      >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>,
 
   document.getElementById("root")
 );
