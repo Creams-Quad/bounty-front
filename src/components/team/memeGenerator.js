190ebd8a5d6b5f2beb2ff2aabbb40217eada0dca
Sunny Lee <sunnylee3712@gmail.com>
2021-08-18 01:27:57 -0700

    got the teams page ready

---
+++ src/components/team/memeGenerator.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@material-ui/core'
+
+
+
+
+export default function MemeGenerator(props)
+
+{
+
+    var items = [
+                {
+                    name: "Mustard Ice-Cream",
+                    description: "I love me some mustard ice-cream to enjoy a cold sandwhich",
+                    image: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2019%2F07%2FIce-Cream-Frenchs-Mustard-FT-Blog0719.jpg"
+                },
+                {
+                    name: "'Toothpaste' and 'Orange Juice'",
+                    description: "A revolutionary idea to clean your teeth as you eat!",
+                    image:"https://i.redd.it/rvxgncp2o3m31.jpg"
+                },
+                {
+                    name: "Ketchap Ice-Cream",
+                    description:"If you have mustard ice-cream, why not ketchap as well?",
+                    image:"https://i.pinimg.com/originals/dd/d7/59/ddd75921ecface8b9dd6c70fd80239a5.jpg",
+                },
+                {
+                    name:"Mac & Cheese Ice-Cream",
+                    description:"The taste of mac and cheese as it melts?",
+                    image: "https://pyxis.nymag.com/v1/imgs/d15/f96/f34967e2e54eec88d908f6abca3697f908-macncheese-icecream.rsquare.w1200.jpg",
+                },
+            ]
+            
+            return (
+                <Carousel >
+                {
+                    items.map( (item, i) => <Item key={i} item={item} /> )
+                }
+                </Carousel>
+                )
+            }
+
+function Item(props)
+{
+    return (
+        <Paper style={{
+               background: "url('https://i.gifer.com/GPkR.gif')"
+                }}>
+            <h2>{props.item.name}</h2>
+            <p style={{
+                color:'white', 
+                marginBottom:'5%', 
+                fontSize: '30pt',
+                marginLeft: '8%'
+                }}>
+                    {props.item.description}</p>
+            
+            <div style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+            }}>
+                <img style={{
+                        width: '60%',
+                        height:'80vh',
+                        }}  
+                        src={props.item.image} 
+                        alt={props.item.name}/>
+            </div>
+        </Paper>
+    )
+}
--- src/components/team/teamPage.js
+++ src/components/team/teamPage.js
@@ -1,14 +1,76 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
+import axios from 'axios';
+
+import TransitionsModal from '../team/modal.js';
+import MediaCard from '../team/teamCard.js';
+import MemeGenerator from './memeGenerator.js';
 
 export default function Team(){ 
+    
+    const [meme, setMemes] = useState(); 
 
-
+    useEffect(()=> {
+        axios({
+            method: 'get',
+            url: 'https://api.imgflip.com/get_memes',
+        })
+        .then( function(response) {
+           setMemes(response.data.data.memes);
+        })
+        .catch( function(err) {
+            console.error(err);
+        })
+    })
 
     return(
-        <div>
+        <div className="container">
             <main>
-                <h1>Hello</h1> 
+                <div>
+                    <TransitionsModal/> 
+                    <div style={{
+                            background: "url('https://c.tenor.com/wcrmL2x1D2gAAAAC/assorted-icecream.gif')",
+                            backgroundSize: 'contain',
+                            opacity: '0.7',
+                            color: 'white'
+                    }}>
+                        <h1 style={{
+                    
+                        }}>Our Mission</h1> 
+                    </div>
+
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems:'center',
+                    }}>
+                        <p style={{marginTop:'3%'}}>A Bounty formed through the love and desire for the ice cream of your dreams. We innovate through imagination and create reality with just a thought.</p>
+                    </div>
+
+                    <div style={{
+                        marginTop: '10%'
+                    }}>
+                        <MemeGenerator  meme={meme}/>
+                    </div>
+
+                    <div style={{
+                        marginTop: '10%',
+                        marginBottom: '10%'
+                    }}>
+                        <p>As you can see from the slide-show up above, there are many different ideas that have lots of potential. We want to understand what users really want in their ice-cream.</p>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent:'center',
+                        alignItems: 'center',
+                        marginBottom:'5%'
+                    }}>
+                        <h2>Our Team</h2>
+                    </div>
+                    <MediaCard/> 
+                </div>
             </main>
+          
         </div>
     ) 
 }
